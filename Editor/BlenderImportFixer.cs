d541d11c835c05d18cf3721bb74c3d37f4d9723e
Umbrason <Umbrason@Outlook.com>
2025-03-11 04:47:31 +0100

    Also bake space transform

--- Editor/BlenderImportFixer.cs
+++ Editor/BlenderImportFixer.cs
@@ -12,6 +12,7 @@ public class BlenderImportFixer
     private const string importerScriptName = "Unity-BlenderToFBX.py";
     private static readonly string relativePathFromAppToImporterScript = Path.Combine("Data", "Tools", importerScriptName);
     private static readonly string bakeAllAnimsRegex = @"bake_anim_use_all_actions=(True|False)";
+    private static readonly string bakeSpaceTransformRegex = @"bake_space_transform=(True|False)";
 
     [InitializeOnLoadMethod]
     static void FixBlenderAnimationImporter()
@@ -31,6 +32,8 @@ If you find this file and it's still relevant please fix the reference in class 
 
         var foundBakeAllAnimsLine = false;
         var foundFalseBakeAllAnimsLine = false;
+        var foundBakeSpaceTransformRegex = false;
+        var foundFalseSpaceTransformRegex = false;
 
         var tempFilePath = Path.GetTempFileName();
         var lines = File.ReadAllLines(pathToImporterScript);
@@ -44,6 +47,29 @@ If you find this file and it's still relevant please fix the reference in class 
             lines[i] = containsFalse ? lines[i].Replace("False", "True") : lines[i];
         }
 
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var match = Regex.Match(lines[i], bakeSpaceTransformRegex);
+            if (!match.Success) continue;
+            foundBakeSpaceTransformRegex = true;
+            var containsFalse = lines[i].Contains("False");
+            foundFalseSpaceTransformRegex |= containsFalse;
+            lines[i] = containsFalse ? lines[i].Replace("False", "True") : lines[i];
+        }
+        if (!foundBakeSpaceTransformRegex)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if(!lines[i].Contains("bpy.ops.export_scene.fbx(filepath=outfile,")) continue;
+                lines[i] = "bpy.ops.export_scene.fbx(filepath=outfile,\nbake_space_transform=True,";
+                break;
+            }
+        }
+        /*
+        axis_forward='-Z',
+        axis_up='Y',
+        bake_space_transform=True,
+        */
         if (!foundBakeAllAnimsLine)
         {
             EditorUtility.DisplayDialog("Could not find any lines to fix", @$"
@@ -53,7 +79,7 @@ In the latter case you can safely remove this package."
             , "OK");
             return;
         }
-        if(!foundFalseBakeAllAnimsLine) return; //nothing to write back
+        if (!foundFalseBakeAllAnimsLine) return; //nothing to write back
         File.WriteAllLines(pathToImporterScript, lines);
     }
 }
